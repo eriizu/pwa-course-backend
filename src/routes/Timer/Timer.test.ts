3cfd1488aa7d246f9b5dc8da93aee81e0f5801e8
Élise Philippe <elise@eiss.fr>
2021-01-23 19:52:17 +0100

    avoid double testing and test the supertest way

--- src/routes/Timer/Timer.test.ts
+++ src/routes/Timer/Timer.test.ts
@@ -6,7 +6,8 @@ let app = express();
 app.use(router);
 
 it("should get timers", async () => {
-  let res = await request(app).get("/");
-  expect(res.status).toBe(200);
+  await request(app).get("/").expect(200);
+  // let res = await request(app).get("/").expect(200);
+  // expect(res.status).toBe(200);
   return;
 });
